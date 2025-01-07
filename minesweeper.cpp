eb0a43fde634618d2aa1258d5443351594a3553d
Isaykin Daniil Timofeevich <k0tk3y@ya.ru>
2025-01-08 00:01:16 +0300

    A lot of updates, but now you can play minesweeper

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -3,23 +3,34 @@
 #include <memory>
 #include <string>
 #include <ctime>
+#include <limits>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::vector;
 using std::shared_ptr;
+using std::string;
 using std::make_shared;
+
+const string alph = "abcdefghijklmnopqrstuvwxyz";
+
+void clearInput() {
+    std::cin.clear(); // Сбрасываем флаги ошибок
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Убираем оставшиеся символы
+}
 
 enum cellType {
     dflt = 0,
     mineFeatured = 1,
-    decorative = 2
+    decorative = 2,
+    opened = 3,
 };
 
 struct Cell {
 private:
     char outputSymbol;
+    bool flag;
     cellType type;
     vector<shared_ptr<Cell>> nearbyCells;
 
@@ -28,6 +39,7 @@ public:
     // Constructors and desctuctors area
 
     Cell(char outputSymbol = ' ', int countOfNearbyCells = 8, cellType type = dflt) {
+        flag = false;
         this->outputSymbol = outputSymbol;
         this->type = type;
         nearbyCells.resize(countOfNearbyCells, nullptr);
@@ -39,6 +51,27 @@ public:
         this->outputSymbol = outputSymbol;
     }
 
+    void setDefaultOutputSymbol() {
+
+        switch (type)
+        {
+        case dflt:
+            outputSymbol = '#';
+            break;
+
+        case mineFeatured:
+            outputSymbol = 'M';
+            break;
+        
+        case opened:
+            outputSymbol = ' ';
+            break;
+        
+        default:
+            break;
+        }
+    }
+
     void setCellType(cellType type) {
         this->type = type;
     }
@@ -46,6 +79,14 @@ public:
     void setNearbyCell(int index, shared_ptr<Cell> pCell) {
         nearbyCells[index] = pCell;
     }   
+
+    void setFlag() {
+        flag = true;
+    }
+
+    void unflag() {
+        flag = false;
+    }
 
     // Getters area
 
@@ -61,25 +102,51 @@ public:
         return nearbyCells.size();
     }
 
+    shared_ptr<Cell> getNearbyCell(int x) {
+        return nearbyCells[x];
+    }
+
+    bool getFlagStatus() {
+        return flag;
+    }
+
     // Another functions
 
     int countNearbyMines() {
         int nearbyMinesCounter = 0;
 
         for(int i = 0; i < nearbyCells.size(); i++) {
-            if(nearbyCells[i].get()->type = mineFeatured)
+            if(nearbyCells[i].get()->getCellType() == mineFeatured)
                 nearbyMinesCounter++;
         }
 
         return nearbyMinesCounter;
     }
 
+    void openCell(int& closedCells) {
+
+        type = opened;
+        closedCells--;
+
+        if(countNearbyMines() != 0) {
+            outputSymbol = 48 + countNearbyMines();
+            return;
+        }
+
+        outputSymbol = ' ';
+
+        for(int i = 0; i < nearbyCells.size(); i++) {
+            if(nearbyCells[i].get()->getCellType() == dflt && !nearbyCells[i].get()->getFlagStatus())
+                nearbyCells[i].get()->openCell(closedCells);
+        }
+    }
 };
 
 struct GameField {
 private:
     short height, width;
     int countOfMines;
+    int closedCells;
     vector<vector<shared_ptr<Cell>>> field;
 
     void generateMines() {
@@ -111,6 +178,8 @@ private:
             h = 26;
             w = 26;
         }
+
+        closedCells = (h * w) - countOfMines;
     }
 
     void initializeField() {
@@ -120,10 +189,10 @@ private:
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 if (i == 0 || i == height - 1) {
-                    field[i][j] = make_shared<Cell>((j == 0 || j == width - 1) ? '+', 3, decorative : '-', 4, decorative);
+                    field[i][j] = make_shared<Cell>((j == 0 || j == width - 1) ? '+', 3, decorative : '-', 5, decorative);
                 }  
                 else if (j == 0 || j == width - 1) field[i][j] = make_shared<Cell>('|', 5, decorative); // '|';
-                else field[i][j] = make_shared<Cell>();
+                else field[i][j] = make_shared<Cell>('#');
             }
         }
 
@@ -135,12 +204,14 @@ private:
 
                 for(int a = -1; a <= 1; a++) {
                     for(int b = -1; b <= 1; b++) {
+                        if(a == 0 && b == 0) continue;
                         int n = i + a,
                             k = j + b;
                         
-                        if(0 <= n > height && 0 <= k > width)
+                        if(n > -1 && n < height && k > -1 && k < width)
                         {
                             field[i][j].get()->setNearbyCell(count, field[n][k]);
+                            count++;
                         }
                     }
                 } 
@@ -151,9 +222,36 @@ private:
     }
 
 public:
-    GameField(short h = 1, short w = 1, int countOfMines = 1) {
-        validateSize(h, w);
+    // GameField(short h = 1, short w = 1, int countOfMines = 1) {
+    //     validateSize(h, w);
+    //     this->countOfMines = countOfMines;
+    //     height = h + 2;
+    //     width = w + 2;
+    //     initializeField();
+    //     cout << "New field generated. Size of field: " << h << 'x' << w << endl;
+    // }
+
+    // Getters area
+
+    short getHeight() {
+        return height;
+    }
+
+    short getWidth() {
+        return width;
+    }
+
+    int getCountOfMines() {
+        return countOfMines;
+    }
+
+    int getClosedCells() {
+        return closedCells;
+    }
+
+    void CreateField(short h = 1, short w = 1, int countOfMines = 1) {
         this->countOfMines = countOfMines;
+        validateSize(h, w);
         height = h + 2;
         width = w + 2;
         initializeField();
@@ -161,7 +259,6 @@ public:
     }
 
     void show_field(bool showMines=false) const {
-        const std::string alph = "abcdefghijklmnopqrstuvwxyz";
         cout << "   ";
         for (int j = 1; j < width - 1; j++) {
             cout << (j - 1 < alph.size() ? alph[j - 1] : ' ');
@@ -179,24 +276,185 @@ public:
                 if(field[i][j].get()->getCellType() != mineFeatured)
                     cout << field[i][j].get()->getOutputSymbol();
                 else if(showMines) cout << field[i][j].get()->getOutputSymbol();
-                else cout << ' ';
+                else cout << '#';
             }
             cout << endl;
         }
     }
+
+    bool openCell(int x, int y) {
+        system("cls");
+        if(x < 1 || x > width - 2 || y < 1 || y > height - 2)
+        {
+            cout << "ERROR! Cell " << alph[x - 1] << y << " is doesnt exist!" << endl;
+            return false;
+        }
+
+        if(field[y][x].get()->getCellType() == mineFeatured) {
+            cout << "You lose!" << endl;
+            return true;
+        }
+
+        if(field[y][x].get()->getCellType() == opened)
+        {
+            cout << "Cell " << alph[x - 1] << y << " is already opened!" << endl;
+            return false;
+        }
+
+        if(field[y][x].get()->getFlagStatus()) {
+            cout << "Cell " << alph[x - 1] << y << " is flagged!" << endl;
+        }
+
+        field[y][x].get()->setCellType(opened);
+        closedCells--;
+
+        int curNearbyMinesCount = field[y][x].get()->countNearbyMines();
+
+        if(curNearbyMinesCount != 0)
+        {
+            field[y][x].get()->setOutputSymbol(curNearbyMinesCount + 48);
+            return false;
+        }
+        
+        field[y][x].get()->setOutputSymbol(' ');
+
+        for(int i = 0; i < field[y][x].get()->getCountOfNearbyCells(); i++) {
+            shared_ptr<Cell> tempCellPTR {field[y][x].get()->getNearbyCell(i)};
+            
+            if(tempCellPTR.get()->getCellType() == dflt && !tempCellPTR.get()->getFlagStatus()) {
+                tempCellPTR.get()->openCell(closedCells);
+            }
+
+            tempCellPTR.reset();
+        }
+
+        return false;
+    }
+
+    bool flagCell(int x, int y) {
+        system("cls");
+        if(field[y][x].get()->getFlagStatus())
+            return false;
+        
+
+        field[y][x].get()->setFlag();
+        field[y][x].get()->setOutputSymbol('F');
+        return true;
+    }
+
+    bool unflagCell(int x, int y) {
+        system("cls");
+        if(!field[y][x].get()->getFlagStatus())
+            return false;
+        
+        field[y][x].get()->unflag();
+        field[y][x].get()->setDefaultOutputSymbol();
+        return true;
+    }
+};
+
+struct game {
+private:
+    GameField gameField;
+
+public:
+
+    void start() {
+        short height, width, amountOfMines;
+        cout << "Input height and width values: ";
+        cin >> height >> width;
+        cout << "Input amount of mines: ";
+        cin >> amountOfMines;
+
+        gameField.CreateField(height, width, amountOfMines);
+        gameField.show_field();
+    }
+
+    bool readCommand() {
+        cout << "> ";
+
+        string command;
+        cin >> command;
+
+        if(command == "open") {
+            bool rValue;
+            char symbol;
+            int number;
+
+            cin >> symbol >> number;
+
+            rValue = gameField.openCell(symbol - 96, number);
+            gameField.show_field();
+
+            if(gameField.getClosedCells() == 0) {
+                cout << "You won!" << endl;
+                rValue = true;
+            }
+
+            return rValue;
+        }
+
+        if(command == "flag") {
+            char symbol;
+            int number;
+            bool rValue;
+
+            cin >> symbol >> number;
+            rValue = gameField.flagCell(symbol - 96, number);
+            gameField.show_field();
+
+            if(rValue)
+                cout << "Cell " << symbol << number << " is flagged!" << endl;
+            else 
+                cout << "Cell " << symbol << number << " is already flagged!" << endl;
+
+            return false;
+        }
+
+        if(command == "unflag") {
+            char symbol;
+            int number;
+            bool rValue;
+
+            cin >> symbol >> number;
+            rValue = gameField.unflagCell(symbol - 96, number);
+            gameField.show_field();
+
+            if(rValue)
+                cout << "Cell " << symbol << number << " is unflagged!" << endl;
+            else 
+                cout << "Cell " << symbol << number << " is already unflagged!" << endl;
+
+            return false;
+        }
+
+        if(command == "help") {
+            system("cls");
+            gameField.show_field();
+
+            printf("COMMANDS: \n help - output this menu\n open xy - open cell with xy coordinated\n           (x - horizontal, y - vertical)\n flag xy - flag cell\n unflag xy - unflag cell\n close - close game\n");
+            return false;
+        }
+
+        if(command == "close") {
+            return true;
+        }
+
+        clearInput();
+        cout << "Unknown command!" << endl;
+        return false;
+    }
 };
 
 int main() {
-    short height, width, amountOfMines;
-    cout << "Input height and width values: ";
-    cin >> height >> width;
-    cout << "Input amount of mines: ";
-    cin >> amountOfMines;
+    game g;
+    g.start();
 
-    system("cls");
-
-    GameField field(height, width, amountOfMines);
-    field.show_field(true);
+    while (1)
+    {
+        bool x = g.readCommand();
+        if(x) break;
+    }
 
     system("pause");
 
